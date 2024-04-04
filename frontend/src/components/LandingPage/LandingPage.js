14b818c5c2645d50326a34409877f3b112974801
Sayan Ghosh <sayanghoshofficial@users.noreply.github.com>
2024-04-04 20:01:37 +0530

    add Landing Page

--- frontend/src/components/LandingPage/LandingPage.js
+++ frontend/src/components/LandingPage/LandingPage.js
@@ -1,15 +1,31 @@
 import React from 'react'
-import { Main } from './index.sc'
-import { Col, Container, Row } from 'react-bootstrap'
+import { ButtonContainer, IntroText, Landingbutton, Main, SubTitle, Title } from './index.sc'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 
 const LandingPage = () => {
     return (
         <Main>
             <Container>
                 <Row>
-                    <Col>
-                    Landing
-                    </Col>
+                    <IntroText>
+                        <div>
+                            <Title>Welcome to Node Zipper</Title>
+                            <SubTitle>One safe place for all your notes.</SubTitle>
+                            <ButtonContainer>
+                                <a href='/login'>
+                                    <Landingbutton>
+                                        Login
+                                    </Landingbutton>
+                                </a>
+                            
+                                <a href='/register'>
+                                    <Landingbutton>
+                                        Register
+                                    </Landingbutton>
+                                </a>
+                            </ButtonContainer>
+                        </div>
+                    </IntroText>
                 </Row>
             </Container>
         </Main>
--- frontend/src/components/LandingPage/index.sc.js
+++ frontend/src/components/LandingPage/index.sc.js
@@ -1,8 +1,64 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Main = styled.div`
 min-height: 90vh;
 display: flex;
 align-items: center;
+background-image: url("../../Assets/background.jpg");
+background-position: center;
+background-size: cover;
+`;
 
+export const Title = styled.h1`
+font-size: 95px;
+`;
+
+export const SubTitle = styled.p`
+font-size:25px;
+padding-top:10px;
+`;
+
+export const IntroText = styled.div`
+width: 100%;
+text-align:center;
+`;
+
+export const ButtonContainer = styled.div`
+ margin-top: 50px;
+  display: flex;
+  justify-content: space-evenly;
+  gap: 1rem;
+  flex-wrap: wrap;
+  /* flex-direction: column; */
+`;
+
+export const Landingbutton = styled.button`
+width: 200px;
+  height: 55px;
+  font-weight: 700;
+
+  &:hover{
+    background-color: var( --bs-blue);
+    color:var(--bs-white);
+  }
+`;
+
+// Define media query styles
+const desktopStyles = css`
+  ButtonContainer {
+    padding: 0 200px;
+  }
+  Title {
+    padding: 0 100px;
+  }
+`;
+
+const mobileStyles = css`
+  Title {
+    font-size: 60px;
+  }
+  Landingbutton {
+    width: 150px;
+    height: 50px;
+  }
 `;
