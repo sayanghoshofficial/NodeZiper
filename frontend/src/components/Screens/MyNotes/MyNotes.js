107693973b664fb4fbe1da3ddd78303aa04eb10b
Sayan Ghosh <sayanghoshofficial@users.noreply.github.com>
2024-04-18 15:13:48 +0530

    Add Accodion to frontend

--- frontend/src/components/Screens/MyNotes/MyNotes.js
+++ frontend/src/components/Screens/MyNotes/MyNotes.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import MainScreen from '../../MainScreen';
 import { Link } from 'react-router-dom';
-import { Button, Card , Badge} from 'react-bootstrap';
+import { Button, Card, Badge, Accordion } from 'react-bootstrap';
 import { notes } from './mockData';
 
 const MyNotes = () => {
@@ -18,28 +18,39 @@ const MyNotes = () => {
           Create New Notes
         </Button>
       </Link>
-      {notes.map(note =>
+      {notes.map((note, i) =>
+        <Accordion flush>
+          <Accordion.Item eventKey={i}>
+            <Card style={{ margin: 10 }}>
+              <Card.Header style={{ display: 'flex' }}>
+                <Accordion.Header style={{width:"100%"}}>
+                  <span style={{ color: 'black', textDecoration: 'none', flex: 1, cursor: 'pointer', alignSelf: 'center', fontSize: 18 }}>
 
-        <Card style={{ margin: 10 }}>
-          <Card.Header style={{ display: 'flex' }}>
-            <span style={{ color: 'black', textDecoration: 'none', flex: 1, cursor: 'pointer', alignSelf: 'center', fontSize: 18 }}>{note?.title}</span>
-            <div>
-              <Button href={`/note/${note?._id}`}>Edit</Button>
-              <Button varient='danger' className='mx-2' onClick={() => deleteHandler(note?._id)}>Delete</Button>
-            </div>
-          </Card.Header>
-          <Card.Body>
-          <h4><Badge varient="success">Catagory - {note?.category}</Badge></h4>
-            <blockquote className="blockquote mb-0">
-              <p>
-                {note?.content}
-              </p>
-              <footer className="blockquote-footer">
-                Someone famous in <cite title="Source Title">Source Title</cite>
-              </footer>
-            </blockquote>
-          </Card.Body>
-        </Card>
+                    {note?.title}
+
+                  </span>
+                  <div>
+                    <Button href={`/note/${note?._id}`}>Edit</Button>
+                    <Button varient='danger' className='mx-2' onClick={() => deleteHandler(note?._id)}>Delete</Button>
+                  </div>
+                </Accordion.Header>
+              </Card.Header>
+              <Card.Body>
+                <Accordion.Body style={{width:"100%"}}>
+                  <h4><Badge varient="success">Catagory - {note?.category}</Badge></h4>
+                  <blockquote className="blockquote mb-0">
+                    <p>
+                      {note?.content}
+                    </p>
+                    <footer className="blockquote-footer">
+                      Someone famous in <cite title="Source Title">Source Title</cite>
+                    </footer>
+                  </blockquote>
+                </Accordion.Body>
+              </Card.Body>
+            </Card>
+          </Accordion.Item>
+        </Accordion>
       )}
     </MainScreen>
   )
