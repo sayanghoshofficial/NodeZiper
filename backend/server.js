382c2d81509296ff55b4a16e166dbf1b9c5aa53c
Sayan Ghosh <sayanghoshofficial@users.noreply.github.com>
2024-04-25 17:51:13 +0530

    changes for production

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const notes = require("./Data/Notes");
+// const notes = require("./Data/Notes");
+const path = require("path")
 const env = require("dotenv");
 const cors = require("cors");
 const connnectDB = require("./config/DB");
@@ -33,6 +34,22 @@ app.get("/", (req, res) => {
 app.use("/api/users", userRoutes)
 app.use("/api/notes", noteRoutes)
 
+// --------------------------deployment------------------------------
+const __dirname1 = path.resolve();
+console.log('__dirname1:', __dirname1);
+console.log('NODE_ENV:', process.env.NODE_ENV);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+// --------------------------deployment------------------------------
 
 
 app.use(notFound);
